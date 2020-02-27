e935d62fc968d20b5ccd8d49360f51009fba7b03
Julia G <galljulia13@gmail.com>
2020-02-27 10:40:56 -0800

    ranking buttons working

--- src/Components/Screens/Rankings/RankingsScreen.js
+++ src/Components/Screens/Rankings/RankingsScreen.js
@@ -1,11 +1,29 @@
 import React, { Component } from 'react';
 import { Container, Header, Content, Card, CardItem, Thumbnail, Button, Text, Title, Left, Body, Right, Segment } from 'native-base';
-import { Image, View } from 'react-native';
+import { Image, View,Dimensions } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import LocalRanksScreen from './LocalRanksScreen';
 import { useNavigation } from '@react-navigation/native';
+var { height, width } = Dimensions.get('window');
+
+
+var images = [
+  require('../../../../assets/dog.jpg'),
+  require('../../../../assets/dog.jpg'),
+  require('../../../../assets/dog.jpg'),
+  require('../../../../assets/dog.jpg'),
+  require('../../../../assets/dog.jpg'),
+  require('../../../../assets/dog.jpg'),
+  require('../../../../assets/dog.jpg'),
+  require('../../../../assets/dog.jpg'),
+  require('../../../../assets/dog.jpg'),
+  require('../../../../assets/dog.jpg'),
+  require('../../../../assets/dog.jpg'),
+  require('../../../../assets/dog.jpg'),
+]
 
 export default class RankingsScreen extends Component {
+
  constructor(props)
  {
    super(props)
@@ -14,6 +32,124 @@ export default class RankingsScreen extends Component {
     }
  }
 
+ segmentClicked(index) {
+  this.setState({
+      activeIndex: index
+  })
+}
+
+  renderSectionOne() {
+    return images.map((image, index) => {
+        return (
+            <View key={index} style={[{ width: (width) / 3 }, { height: (width) / 3 }, { marginBottom: 2 }, index % 3 !== 0 ? { paddingLeft: 2 } : { paddingLeft: 0 }]}>
+                <Image style={{
+                    flex: 1,
+                    alignSelf: 'stretch',
+                    width: undefined,
+                    height: undefined,
+
+                }}
+                    source={image}>
+                </Image>
+
+            </View>
+        )
+    })
+  }
+
+    renderSection() {
+
+      if (this.state.activeIndex == 0) {
+  
+          return (
+            <View style={{ flexDirection: 'row', flexWrap: 'wrap' }}>
+  
+            {this.renderSectionOne()}
+          </View>
+          )
+      }
+      else if (this.state.activeIndex == 1) {
+          return (
+          <View style={{paddingBottom:2, alignContent:'stretch'}}> 
+          <Card>
+            <CardItem>
+              <Left>
+                <View  style={{ flexDirection: 'row' }}>
+                  {/*Mini profile pic and misc info*/}
+                    <View 
+                      style={{flex: 1, alignItems: 'left', justifyContent: 'flex-start', flexDirection: 'row' }}>
+              
+                      <Image source={require('../../../../assets/dog.jpg')}
+                      style={{ width: 50, height: 50, borderRadius: 37.5 }} />
+  
+                      <View  style={{alignItems: 'flex-start', flexDirection: 'column', justifyContent:'space-around'}} >
+                        <Button small dark transparent>
+                          <Text>Courage T. Dog</Text>
+                        </Button>
+                        <Text style={{ alignSelf:'flex-start', fontSize: 10,  paddingTop:0, padding:6, color: 'grey'}}>Rank #3 Global</Text>
+                      </View>
+                  </View>
+                </View>
+              </Left>
+              {/** this is the 'up-vote' button */}
+              <Right>
+              <View 
+                      style={{paddingTop:5,flex:1, alignItems: 'flex-end',  justifyContent: 'flex-start', flexDirection: 'column' }}>
+              
+                     <Button iconLeft small transparent>
+                        <Icon name="arrow-circle-up" style={{ color: '#829399' }} size={30}></Icon>
+                     </Button>
+                        <Text style={{ alignSelf: 'flex-end', fontSize: 10,  paddingRight:3, paddingTop:3, color: 'grey'}}>300</Text>
+                  </View>
+              </Right>
+            </CardItem>
+  
+            {/*This is where card content goes*/}
+            <CardItem cardBody>
+            <Image style={{width: 370, height: 200}}
+                  source={require('../../../../assets/dog.jpg')}
+            />
+            </CardItem>
+            {/*Bottom of the card (left side) */}
+            <CardItem>
+              {/*Left side contains hearts/comments buttons */}
+              <Left>
+                <View style={{flexDirection:'row'}}>
+                <Button transparent>
+                <Icon name="heart" style={{ color: '#7BCACE' }} size={25}></Icon>
+                  <Text style={{padding:0 ,color:'black'}}>4 million</Text>
+                </Button>
+              
+                <Button transparent>
+                  <Icon name="comment" style={{ color: '#7BCACE' }} size={25}></Icon>
+                  <Text style={{padding:0 ,color:'black'}}>1.2 million</Text>
+                </Button>
+                </View>
+              </Left>
+  
+              {/**Just the downvote button (right side) */}
+              <Right>
+              <View 
+                      style={{paddingTop:5,flex:1, alignItems: 'flex-end',  justifyContent: 'flex-start', flexDirection: 'column' }}>
+              
+                     <Button iconLeft small transparent>
+                        <Icon name="arrow-circle-down" style={{ color: '#829399' }} size={30}></Icon>
+                     </Button>
+                  </View>
+              </Right>
+            </CardItem>
+          </Card>
+          </View>
+          )
+      }
+      else if (this.state.activeIndex == 2) {
+        return(
+        <View style={{ flexDirection: 'row', flexWrap: 'wrap' }}>
+  
+        {this.renderSectionOne()}
+        </View>)
+      }
+  }
   render(){
   return (
     <Container style={{backgroundColor:"#EFFCCC"}}>
@@ -42,16 +178,16 @@ export default class RankingsScreen extends Component {
      * to change the content area of this screen
     */}
     <Segment>
-      <Button first style={{borderColor:'#545F66', }} >
+      <Button first onPress = {() => this.segmentClicked(0)} active={this.state.activeIndex ==0}  style={{borderColor:'#545F66', }} >
        <Text style={{color:'#545F66'}}>Local</Text> 
         
       </Button>
 
-      <Button style={{borderColor:'#545F66', }}>
+      <Button onPress = {() => this.segmentClicked(1)} active={this.state.activeIndex ==1} style={{borderColor:'#545F66', }}>
         <Text style={{color:'#545F66'}}>Regional</Text>
       </Button>
 
-      <Button last style={{borderColor:'#545F66', }}>
+      <Button onPress = {() => this.segmentClicked(2)} active={this.state.activeIndex ==2} last style={{borderColor:'#545F66', }}>
         <Text style={{color:'#545F66'}}>Global</Text>
       </Button>      
     </Segment>
@@ -183,6 +319,7 @@ export default class RankingsScreen extends Component {
           </CardItem> 
         </Card>
         </View>
+        {this.renderSection()}
     </Content>
     
 </Container>
