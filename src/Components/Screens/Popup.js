5c25dc8ef630265e9c45ec398820d62f8a491e6d
NW <nathan@nnorris.com>
2020-03-24 16:20:43 -0500

    create dad profile: input styling

--- src/Components/Screens/Popup.js
+++ src/Components/Screens/Popup.js
@@ -149,6 +149,21 @@ export default class Popup extends Component {
     }
 
     render() {
+        var inputBoxStyle = {
+          backgroundColor: "#f3fcd9",
+          height: 45,
+          marginTop: 10,
+          width: 300,
+          marginBottom: 15,
+          borderLeftWidth: 5,
+          borderRadius: 0,
+          padding: 10,
+          fontSize: 18,
+          textAlign: "center",
+          borderColor: "#dae6ba",
+          color: "#aaa"
+        }
+
         return (
             <Modal
                 visible = {this.state.visibility}>
@@ -158,20 +173,20 @@ export default class Popup extends Component {
                         style={{ width: "15%", alignItems: "center" }}>
                         <Text style={{ fontWeight: "bold", fontSize: 30}}>{"<"}</Text>
                     </Button>
-                    <Text style={{ marginTop: 50, fontSize: 30, fontWeight: "bold" }}>Create your dad profile here!</Text>
+                    <Text style={{ marginTop: 20, fontSize: 30, fontWeight: "bold" }}>Create your dad profile here!</Text>
                     <ScrollView style={{ width: "100%" }}>
                         <View style={{ marginTop: 25, justifyContent: "center", alignItems: "center", overflow: "auto"}}>
                             <Text style={{ fontWeight: "bold" }}>First Name</Text>
-                            <TextInput placeholder="First Name Here"
-                                placeholderTextColor="black"
+                            <TextInput placeholder="First Name"
+                                placeholderTextColor="#ccc"
                                 onChangeText={(text) => this.setState({ firstName: text })}
-                                style={{ backgroundColor: "white", height: 20, width: 300, marginBottom: 15, borderWidth: 1, borderRadius: 20, fontSize: 12, textAlign: "center", borderColor: "black" }}/>
+                                style={inputBoxStyle}/>
 
                             <Text style={{ fontWeight: "bold" }}>Last Name</Text>
-                            <TextInput placeholder="Last Name Here"
-                                placeholderTextColor="black"
+                            <TextInput placeholder="Last Name"
+                                placeholderTextColor="#ccc"
                                 onChangeText={(text) => this.setState({ lastName: text })}
-                                style={{ backgroundColor: "white", height: 20, width: 300, marginBottom: 40, borderWidth: 1, borderRadius: 20, fontSize: 12, textAlign: "center", borderColor: "black" }}/>
+                                style={inputBoxStyle}/>
 
                             <Text style={{ fontWeight: "bold", fontSize: 25, marginBottom: 20 }}>Skills Ratings</Text>
 
@@ -218,19 +233,19 @@ export default class Popup extends Component {
                             <TextInput placeholder="Country Here"
                                 placeholderTextColor="black"
                                 onChangeText={(text) => this.setState({ country: text })}
-                                style={{ backgroundColor: "white", height: 20, width: 300, marginBottom: 40, borderWidth: 1, borderRadius: 20, fontSize: 12, textAlign: "center", borderColor: "black" }}/>
+                                style={inputBoxStyle}/>
 
                             <Text style={{ fontWeight: "bold" }}>Region</Text>
                             <TextInput placeholder=" Region Here"
                                 placeholderTextColor="black"
                                 onChangeText={(text) => this.setState({ region: text })}
-                                style={{ backgroundColor: "white", height: 20, width: 300, marginBottom: 40, borderWidth: 1, borderRadius: 20, fontSize: 12, textAlign: "center", borderColor: "black" }}/>
+                                style={inputBoxStyle}/>
 
                             <Text style={{ fontWeight: "bold" }}>Zip Code</Text>
                             <TextInput placeholder="Zip Code Here"
                                 placeholderTextColor="black"
                                 onChangeText={(text) => this.setState({ zip: text })}
-                                style={{ backgroundColor: "white", height: 20, width: 300, marginBottom: 40, borderWidth: 1, borderRadius: 20, fontSize: 12, textAlign: "center", borderColor: "black" }}/>
+                                style={inputBoxStyle}/>
 
                             <Body style={{ justifyContent: "center", alignItems: "center"}}>
                                 <Button block
